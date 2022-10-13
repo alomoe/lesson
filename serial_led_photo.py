e8d192d076ac22151df064a829e7ec9c9263f495
alomoe <76548063+alomoe@users.noreply.github.com>
2022-10-13 21:31:10 +0800

    Add files via upload

--- serial_led_photo.py
+++ serial_led_photo.py
@@ -1,24 +1,39 @@
 import serial
 import time
-
-lengths={'s':3, 'u':0, 'd':0}
+lenghts = {'s':3, 'u':0,'d':0}
+values = []
 
 def get_connection(port):
-  ser=serial.Serial(port, timeout=1)
-  return ser
+    ser = serial.Serial(port,timeout=1)
+    return ser
 
-def send(ser, message, mesg_len):
-  ser.write(message)
-  time.sleep(0.1)
-  if mesg_len !=0:
-    data = ser.read(mesg_len)
-    result = data.decode()
-    result = result.strip()
-    print(result)
+def send(ser,message, mesg_len):
+    ser.write(message)
+    time.sleep(0.005)
+    if mesg_len != 0:
+        data = ser.readline()
+        result = data.decode()
+        result = result.strip()
+        return (result)
 
 if __name__ == '__main__':
-    ser=get_connection("COM9")
+    ser = get_connection("COM5")
     while True:
-        inp = input("Enter command:")
-        length = lengths.get(inp,17)
-        send(ser, inp.encode(), length)
+        print("Enter command:")      
+        inp = input()
+        if inp == "u" or inp == "d" :
+            send(ser, inp.encode(), lenghts[inp])
+        elif inp == "auto":
+            while True:
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    val = int(val)
+                    print(val)
+                    if val > 900:
+                        send(ser, 'd'.encode(), 0)
+                    else:
+                        send(ser, 'u'.encode(), 0)
+
+        else:
+            print("Неизваестная команда")
